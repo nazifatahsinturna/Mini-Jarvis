63ae770733268b1353a4ef85b833e8cb4ad273dd
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2026-01-16 18:42:10 -0500

    model loaded

--- JARVIS.py
+++ JARVIS.py
@@ -7,3 +7,8 @@ import os
 print("Loading your AI Assistant (This might take a minute)")
 
 #loading the AI Model
+model_name ="google/flan-t5-large"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+print("Assitant loaded! Ready to chat.")
+
